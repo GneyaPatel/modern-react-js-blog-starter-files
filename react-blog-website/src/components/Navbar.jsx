0e0eb01eaa8cea541b81e1c0abc55abaa0429aeb
GneyaPatel <88178249+GneyaPatel@users.noreply.github.com>
2024-11-19 14:55:02 +0530

    commit

--- react-blog-website/src/components/Navbar.jsx
+++ react-blog-website/src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+
+//react icons
+import { FaFacebook, FaTwitter, FaBars  } from "react-icons/fa";
+ 
 
 const Navbar = () => {
     //navitems
@@ -12,22 +16,29 @@ const Navbar = () => {
 ,
     ]
   return (
-    <header className='bg-black'>
-      <nav className='px-4 py-4'>
+    <header className='bg-black text-white'>
+      <nav className='px-4 py-4 max-w-7xl mx-0'>
         <a href="/"  className='text-xl font-bold text-white'>Design<span className='text-orange-500'>DK</span></a>
 
         {/* navitems for lg devices*/}
         <ul className='md:flex gap-12 text-lg hidden'>
           {
-            navItems.map(({path,link}) => <li className='text-white' key={path}>
-              <NavLink to={path}>{Link}</NavLink>
-            </li>)
+           navItems.map(({path,link})=><li className='text-white' key={path}>
+              <NavLink to={path}>{link}</NavLink>
+           </li>)
           }
         </ul>
 
         {/*menu icons */}
+        <div className='text-white lg:flex gap-4 items-center hidden'>
+          <a href="/" className='hover:text-orange-500'><FaFacebook/></a>
+          <a href="/" className='hover:text-orange-500'><FaTwitter  /></a>
+          <button className='bg-orange-500 px-6 py-2 font-medium round hover:bg-white hover:text-orange-500 transition-all duration-200 ease-in'>Log-in</button>
+        </div>
+
+        {/*mobile menu btn, display mobile screen */}
         <div>
-          <a href="/ "></a>
+          <button className='cursor-pointer'><FaBars className='w-5 h-5'/></button>
         </div>
       </nav>
     </header>
