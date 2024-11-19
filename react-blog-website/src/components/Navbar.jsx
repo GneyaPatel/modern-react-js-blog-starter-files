78af99c9911911d2f926a3a8c819e3d9555a6d92
GneyaPatel <88178249+GneyaPatel@users.noreply.github.com>
2024-11-19 14:18:33 +0530

    commit

---
+++ react-blog-website/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+    //navitems
+    const navItems =[
+        {path:"/", link: "Home" },
+        {path:"/about", link: "About"},
+        {path:"/blogs", link: "Blogs"},
+        {path:"/services", link: "Services"},
+        {path:"/contact", link: "Contact"}
+,
+    ]
+  return (
+    <header className='bg-black'>
+      <nav className='px-4 py-4'>
+        <a href="/"  className='text-xl font-bold text-white'>Design<span className='text-orange-500'>DK</span></a>
+
+        {/* navitems for lg devices*/}
+        <ul className='md:flex gap-12 text-lg hidden'>
+          {
+            navItems.map(({path,link}) => <li className='text-white' key={path}>
+              <NavLink to={path}>{Link}</NavLink>
+            </li>)
+          }
+        </ul>
+
+        {/*menu icons */}
+        <div>
+          <a href="/ "></a>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
