e95c85b5228a70a3f458c068244ea7546b993f51
GneyaPatel <88178249+GneyaPatel@users.noreply.github.com>
2024-11-20 22:20:44 +0530

    commit

--- react-blog-website/src/components/BlogPage.jsx
+++ react-blog-website/src/components/BlogPage.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import BlogCards from './BlogCards';
+import Pageination from './Pageination';
 
 const BlogPage = () => {
     const [blogs,setBlogs] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageSize = 12 //blogs per page
+    const [selectedCategory, setSelectedCetegory] = useState(null);
+    const [activeCategory, setActiveCategory] = useState(null);
 
     useEffect(() => {
         async function fetchBlogs() {
-            let url = `http://localhost:5000/blogs`
+            let url = `http://localhost:5000/blogs?page=${currentPage}&limit=${pageSize}`;
+
+            //filter by category
+            if(selectedCategory){
+                url +=`&category=${selectedCategory}`;
+            } 
 
             const response = await fetch(url);
             const data =await response.json()
@@ -14,8 +24,21 @@ const BlogPage = () => {
             setBlogs(data);
         }
         fetchBlogs();
-    }, [])
-    
+    }, [currentPage, pageSize, selectedCategory])
+
+    // page changing btn
+    const handlePageChange = (pageNumber) =>{
+        setCurrentPage(pageNumber);
+    }
+
+    const handleCategoryChange = (category) => {
+        setSelectedCetegory(category);
+        setCurrentPage(1);
+        setActiveCategory(category);
+    }
+
+
+
   return (
     <div>
         {/*caterogy section*/}
@@ -23,11 +46,11 @@ const BlogPage = () => {
 
         {/*blogcards section */}
         <div>
-            <BlogCards blogs={blogs} />
+            <BlogCards blogs={blogs} currentPage={currentPage} selectedCategory={selectedCategory} pageSize={pageSize}/>
         </div>
 
         {/*pagination section */}
-        <div>Pagination</div>
+        <div><Pageination onPageChange={handleCategoryChange} currentPage={currentPage} blogs={blogs} pageSize={pageSize}/></div>
     </div>
   )
 }
---
+++ react-blog-website/src/components/Pageination.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Pageination = ({onPageChange, currentPage, blogs, pageSize}) => {
+  const totalPages = Math.ceil(blogs.length / pageSize);
+  //console.log(totalPages)
+  const renderPaginationLink = () => {
+    return Array.from({length: totalPages}, (_, i) => i + 1).map((pageNumber) => (
+        <li className={pageNumber === currentPage ? "activePagination" : ""} key={pageNumber}>
+            <a href="#" onClick={() =>onPageChange(pageNumber)}>{pageNumber}</a>
+        </li>
+    ))
+  }
+    return (
+        <ul>
+            <li>
+                <button onClick={()=>onPageChange(currentPage -1)} disabled={currentPage ===1}> Pervious</button>
+            </li>
+            <div>
+                {renderPaginationLink()}
+            </div>
+            <li>
+                <button>Next</button>
+            </li>
+        </ul>
+  )
+}
+
+export default Pageination
