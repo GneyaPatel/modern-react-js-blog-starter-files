7236a8a4ced6d7819b1bf43168273db347c11e4d
GneyaPatel <88178249+GneyaPatel@users.noreply.github.com>
2024-11-20 13:46:36 +0530

    commit

--- react-blog-website/src/components/Navbar.jsx
+++ react-blog-website/src/components/Navbar.jsx
@@ -54,9 +54,9 @@ const Navbar = () => {
 
       {/*menu items only for mobile*/}
       <div>
-      <ul className='md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white'>
+      <ul className={'md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white ${isMenuOpen ? "fixed top-0 left-0 w-full transition-all ease-out duration-150 " : "hidden"} '}>
           {
-           navItems.map(({path,link})=><li className='text-white' key={path}>
+           navItems.map(({path,link})=><li className='text-black' key={path}>
               <NavLink to={path}>{link}</NavLink>
            </li>)
           }
