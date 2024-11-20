fe57343bf708e22408cfaa88522ea272abf79e86
GneyaPatel <88178249+GneyaPatel@users.noreply.github.com>
2024-11-20 14:27:20 +0530

    commit

--- react-blog-website/src/components/Banner.jsx
+++ react-blog-website/src/components/Banner.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {FaArrowRight} from "react-icons/fa6"
 
 const Banner = () => {
   return (
@@ -6,6 +8,9 @@ const Banner = () => {
         <div className='text-white text-center'>
             <h1 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Welcome to Our Blog</h1>
             <p className='text-gray-100 lg:w-3/5 mx-auto font-primary '>Start your blog today and join a community of writers and readers who are passionate about sharing their stories and ideas.we offer everything you need to get started, from helpful tips and tutorials.</p>
+            <div>
+                <Link to="/" className='font-medium hover:text-orange-500 inline-flex item-center py-2'>Learn More<FaArrowRight className='mt-1 ml-2'/></Link>
+            </div>
         </div>
     </div>
   )
