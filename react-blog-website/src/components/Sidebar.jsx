7e5ad572ace35263dbc0994e880b91c5fd266cce
GneyaPatel <88178249+GneyaPatel@users.noreply.github.com>
2024-11-23 17:57:19 +0530

    commit

--- react-blog-website/src/components/Sidebar.jsx
+++ react-blog-website/src/components/Sidebar.jsx
@@ -14,9 +14,9 @@ const Sidebar = () => {
             <h3 className='text-2xl font-semibold px-4'>Latest Blogs</h3>
             <div>
                 {
-                    popularBlogs.map(blog => <div key={blog.id}>
-                        <h4>{blog.title}</h4>
-                        <Link to="/" className='font-medim hover:text-orange-500 inline-flex item-center py-1'>Read More <FaArrowRight className='mt-1 ml-2'/></Link>
+                    popularBlogs.slice(0,5).map(blog => <div key={blog.id} className='my-5 border-b-2 border-spacing-2 px-4'>
+                        <h4 className='font-medium mb-2'>{blog.title}</h4>
+                        <Link to="/" className='text-base pb-2 hover:text-orange-500 inline-flex item-center py-1'>Read More <FaArrowRight className='mt-1 ml-2'/></Link>
                     </div>)
 
                 }
